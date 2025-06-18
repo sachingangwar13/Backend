49c593fe8554378c0cef8a227797ded82ef24f23
sachingangwar13 <sss97581300@gmail.com>
2025-06-18 16:17:37 +0530

    added logger

--- 02_Express/index.js
+++ 02_Express/index.js
@@ -1,9 +1,29 @@
 import express from 'express'
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express()
 const port = 3000
+const morganFormat = ":method :url :status :response-time ms";
+
 
 app.use(express.json())
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 let friendsName = []
 let nextIndex = 1
